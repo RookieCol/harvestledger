4a814e19a50e70bb0fcf70f35931d6b6540ba8be
rookie <juan.diegoam@hotmail.com>
2023-11-03 18:53:07 -0500

    :building_construction: API Prefix

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -4,6 +4,7 @@ import { GatewayModule } from './gateway.module';
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
   app.enableCors();
+  app.setGlobalPrefix('api/v1');
   await app.listen(5000);
 }
 bootstrap();
