898610b86255b02833b81c8f0b8f775033f89f5e
garyfernandez <jotagary25@protonmail.com>
2023-11-09 14:26:40 -0400

    feat: updated crop entity with 2 new propertys

--- libs/common/src/entities/crops.entity.ts
+++ libs/common/src/entities/crops.entity.ts
@@ -7,22 +7,37 @@ import { HarvestEntity } from './harvest.entity';
 export class CropEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   name: string;
+  
   @Column()
   product: string;
+  
   @Column()
   size: number;
+  
   @Column()
   location: string;
+  
   @Column()
   sowingDate: string;
+  
   @Column()
   plants: number;
+
+  @Column({ default: null })
+  metadataLink: string;
+
+  @Column({ default: null })
+  nftId: number;
+  
   @ManyToOne(() => FarmEntity, (farm) => farm.id)
   farm: FarmEntity;
+  
   @OneToMany(() => ActivitiesEntity, (activities) => activities.id)
   activities: ActivitiesEntity[];
+  
   @OneToMany(() => HarvestEntity, (harvest) => harvest.id)
   harvest: HarvestEntity[];
 }
